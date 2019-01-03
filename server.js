9a967b132428a3c7b94a198666136093820586ed
Hwan Seob Lee <bestseob93@gmail.com>
2019-01-03 23:06:05 +0900

    history html added

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Hello World');
+app.get('/history1', (req, res) => {
+	fs.readFile('./history1.html', (err, data) => {
+		res.writeHead(200, {
+			'Content-Type': 'text/html'
+		});
+		res.end(data);
+	});
+});
+
+app.get('history2', (req, res) => {
+	fs.readFile('./history2.html', (err, data) => {
+		res.writeHead(200, {
+			'Content-Type': 'text/html'
+		});
+		res.end(data);
+	});
+});
+
+app.get('history3', (req, res) => {
+	fs.readFile('./history3.html', (err, data) => {
+		res.writeHead(200, {
+			'Content-Type': 'text/html'
+		});
+		res.end(data);
+	});
 });
 
 app.listen(3000, () => {
