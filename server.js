d5471174af00838308d9052238c84b0e5ed9057e
Hwan Seob Lee <bestseob93@gmail.com>
2019-01-03 23:11:32 +0900

    / 빼먹음

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ app.get('/history1', (req, res) => {
 	res.sendFile(path.join(__dirname+'/history1.html'));
 });
 
-app.get('history2', (req, res) => {
+app.get('/history2', (req, res) => {
 	res.sendFile(path.join(__dirname+'/history2.html'));
 });
 
-app.get('history3', (req, res) => {
+app.get('/history3', (req, res) => {
 	res.sendFile(path.join(__dirname+'/history3.html'));
 });
 
