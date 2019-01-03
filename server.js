ee4bac12b9ed791f86877bb5a02b54f920e11315
Hwan Seob Lee <bestseob93@gmail.com>
2019-01-03 23:07:58 +0900

    / 추가

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-app.get('/history1', (req, res) => {
+app.get('/', (req, res) => {
 	fs.readFile('./history1.html', (err, data) => {
 		res.writeHead(200, {
 			'Content-Type': 'text/html'
